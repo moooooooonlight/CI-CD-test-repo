8b79a52add70e135177e611e757bd4a6d1b1d3e9
권용민 <kwonym0914@gmail.com>
2025-05-03 00:11:41 +0900

    feat: project 생성

---
+++ ink3/src/main/java/com/nhnacademy/ink3/Ink3Application.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.ink3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ink3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ink3Application.class, args);
+    }
+
+}
