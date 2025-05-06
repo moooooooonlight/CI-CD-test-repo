fd9fc26cc8048aa4d0d254d6130694ffd0b89726
권용민 <kwonym0914@gmail.com>
2025-05-06 11:04:21 +0900

    feat: init

---
+++ src/main/java/com/nhnacademy/eurakaapi/EurakaApiApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.eurakaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurakaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurakaApiApplication.class, args);
+    }
+
+}
